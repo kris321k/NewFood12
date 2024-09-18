4e7baccbfb3bcc7ef15fa35dd3ba91866e904398
kris321k <rajendranaik6622@gmail.com>
2024-09-18 09:48:18 +0530

    updatedcode

--- foodbyte/food/models.py
+++ foodbyte/food/models.py
@@ -20,6 +20,7 @@ class Person(AbstractUser):
 
 class Category(models.Model):
     category_name = models.CharField(max_length=20, unique=True)
+    category_img=models.ImageField(upload_to='images/')
 
     def __str__(self):
         return self.category_name
